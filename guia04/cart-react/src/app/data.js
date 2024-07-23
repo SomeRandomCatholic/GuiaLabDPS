92cd1a8c2b9c511f71a4df53767b460c79e12a5e
SomeRandomCatholic <christoamilcruz@gmail.com>
2024-07-23 08:57:25 -0600

    Parte de la discusión

--- guia04/cart-react/src/app/data.js
+++ guia04/cart-react/src/app/data.js
@@ -3,6 +3,12 @@ export const data = [
    {id:2, title:'El Señor de los Anillos (Trilogía)', price:190, quantity:1, urlImage:'https://proassetspdlcom.cdnstatics2.com/usuaris/libros/fotos/358/original/portada_pack-trilogia-el-senor-de-los-anillos_j-r-r-tolkien_202206071544.jpg'},
    {id:3, title:'Cuentos de Barro', price:30, quantity:1, urlImage:'https://www.librosdelaballena.com/wp-content/uploads/2020/05/cuentos-barro-244x300.png'},
    {id:4, title:'Tierra de Infancia', price:30, quantity:1, urlImage:'https://marketsvstg.blob.core.windows.net/marketsv/0011705_tierra-de-infancia_510.jpeg'},
-   {id:5, title:'Harry Potter Pack', price:390, quantity:1, urlImage:'https://contentv2.tap-commerce.com/cover/large/9789878000473_1.jpg?id_com=1113'}
+   {id:5, title:'Harry Potter Pack', price:390, quantity:1, urlImage:'https://contentv2.tap-commerce.com/cover/large/9789878000473_1.jpg?id_com=1113'},
+   {id:6, title:'La Biblia Católica', price:15, quantity:1, urlImage:'https://ss365.liverpool.com.mx/xl/1116603710.jpg'},
+   {id:7, title:'El Hobbit', price:16, quantity:1, urlImage:'https://proassetspdlcom.cdnstatics2.com/usuaris/libros/fotos/348/original/portada_el-hobbit-ne_j-r-r-tolkien_202202140958.jpg'},
+   {id:8, title:'Narnia: la saga completa', price:40, quantity:1, urlImage:'https://payhip.com/cdn-cgi/image/format=auto/https://pe56d.s3.amazonaws.com/o_1fnjrm6lo1lo8cs31n0d1mrt11utm.jpg'},
+   {id:9, title:'Suma Teológica', price:50, quantity:1, urlImage:'https://pictures.abebooks.com/inventory/31251110367.jpg'},
+   {id:10, title:'La Isla del Tesoro', price:6, quantity:1, urlImage:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrkRuQVM0Voj0ySO45k92fI4MdfjO5Jf_5bw&s'},
+
 ];
 
--- guia04/cart-react/src/components/Headers.jsx
+++ guia04/cart-react/src/components/Headers.jsx
@@ -1,75 +1,109 @@
-"use client"
+"use client";
 import React from "react";
 import { useState } from "react";
-export const Headers = ({allProducts, setAllProducts, total, countProducts, setCountProducts, setTotal,}) =>{
-    
-    const [active, setActive] = useState(false);
+export const Headers = ({
+  allProducts,
+  setAllProducts,
+  total,
+  countProducts,
+  setCountProducts,
+  setTotal,
+}) => {
+  const [active, setActive] = useState(false);
 
-    const onDeleteProduct = product =>{
-        const results = allProducts.filter(item => item.id !== product.id);
+  const onDeleteProduct = (product) => {
+    const results = allProducts.filter((item) => item.id !== product.id);
 
-        setTotal(total - product.price * product.quantity);
-        setCountProducts(countProducts - product.quantity);
-        setAllProducts(results);
-    };
+    setTotal(total - product.price * product.quantity);
+    setCountProducts(countProducts - product.quantity);
+    setAllProducts(results);
+  };
 
-    const onCleanCart = () =>{
-        setAllProducts([]);
-        setTotal(0);
-        setCountProducts(0);
-    }
-    return(
-        <header>
-            <h1>Tienda de Libros</h1>
-            <div className="container-icon">
-                <div className="container-cart-icon" onClick={() =>{setActive(!active)}}>
-                    <img src="https://static.vecteezy.com/system/resources/previews/000/643/145/non_2x/vector-shopping-cart-icon.jpg" alt="carrito" className="icon-cart"></img>
-                    <div className="count-products">
-                        <span id="contador-productos">{countProducts}</span>
+  const onCleanCart = () => {
+    setAllProducts([]);
+    setTotal(0);
+    setCountProducts(0);
+  };
+  return (
+    <header>
+      <h1>Tienda de Libros</h1>
+      <div className="container-icon">
+        <div
+          className="container-cart-icon"
+          onClick={() => {
+            setActive(!active);
+          }}
+        >
+          <img
+            src="https://static.vecteezy.com/system/resources/previews/000/643/145/non_2x/vector-shopping-cart-icon.jpg"
+            alt="carrito"
+            className="icon-cart"
+          ></img>
+          <div className="count-products">
+            <span id="contador-productos">{countProducts}</span>
+          </div>
+        </div>
+
+        <div
+          className={`container-cart-products ${active ? "" : "hidden-cart"}`}
+        >
+          {allProducts.length ? (
+            <>
+              <div className="row-product">
+                {allProducts.map((product) => (
+                  <div className="cart-product" key={product.id}>
+                    <div className="info-cart-product">
+                      <span className="cantidad-producto-carrito">
+                        {product.quantity}
+                      </span>
+                      <span className="mi-Imagen">
+                        <img
+                          src={product.urlImage}
+                          alt={`imagen del ${product.title}`}
+                        ></img>
+                      </span>
+                      <p className="titulo-producto-carrito">{product.title}</p>
+                      <span className="precio-producto-carrito">
+                        ${product.price}
+                      </span>
                     </div>
-                </div>
+                    <img
+                      src="https://static.vecteezy.com/system/resources/previews/018/887/462/original/signs-close-icon-png.png"
+                      alt="cerrar"
+                      className="icon-close"
+                      onClick={() => {
+                        if (
+                          confirm(
+                            "¿Estás seguro de querer eliminar este producto?"
+                          )
+                        )
+                          onDeleteProduct(product);
+                      }}
+                    ></img>
+                  </div>
+                ))}
+              </div>
 
-                <div className={`container-cart-products ${active ? '': 'hidden-cart'}`}>
-                    {allProducts.length ? (
-                        <>
-                        <div className="row-product">
-                            {allProducts.map(product =>(                              
-                                <div className="cart-product" key={product.id}>
-                                    <div className="info-cart-product">
-                                        <span className="cantidad-producto-carrito">
-                                            {product.quantity}
-                                        </span>
-                                        <span className="mi-Imagen">
-                                            <img src={product.urlImage} alt={`imagen del ${product.title}`}></img>
-                                        </span>
-                                        <p className="titulo-producto-carrito">
-                                            {product.title}
-                                        </p>
-                                        <span className="precio-producto-carrito">
-                                            ${product.price}
-                                        </span>
-                                    </div>
-                                    <img src="https://static.vecteezy.com/system/resources/previews/018/887/462/original/signs-close-icon-png.png" alt="cerrar" className="icon-close" onClick={ ()=>{
-                                        onDeleteProduct(product)}
-                                        }></img>
-                                </div>
-                            ))}
-                        </div>
+              <div className="cart-total">
+                <h3>Total:</h3>
+                <span className="total-pagar">${total}</span>
+              </div>
 
-                        <div className="cart-total">
-                            <h3>Total:</h3>
-                            <span className="total-pagar">${total}</span>
-                        </div>
-
-                        <button className="btn-clear-all" onClick={onCleanCart}>Vaciar Carrito</button>
-                        </>
-                    ) 
-                    : 
-                    (<p className="cart-empty">El carrto está vacío</p>)
-                    }
-                    
-                </div>
-            </div>
-        </header>
-    );
-}
+              <button
+                className="btn-clear-all"
+                onClick={() => {
+                  if (confirm("¿Estás seguro de querer vaciar el carrito?"))
+                    onCleanCart();
+                }}
+              >
+                Vaciar Carrito
+              </button>
+            </>
+          ) : (
+            <p className="cart-empty">El carrto está vacío</p>
+          )}
+        </div>
+      </div>
+    </header>
+  );
+};
