d5f7d1cb888eadb5f1861ada1f720ebaa26fc381
SomeRandomCatholic <christoamilcruz@gmail.com>
2024-07-24 22:53:19 -0600

    Ultimo ejercicio complementario

--- guia04/cart-react/src/components/ProductList.jsx
+++ guia04/cart-react/src/components/ProductList.jsx
@@ -12,7 +12,7 @@ export const ProductList = ({
   const onAddProduct = (product) => {
     // Busca si el item ya había sido añadido al carrito de compras
     if (allProducts.find((item) => item.id === product.id)) {
-      const products = allProducts.map(item => 
+      const products = allProducts.map(item =>
         item.id === product.id
           ? { ...item, quantity: item.quantity + 1 }
           : item
@@ -28,13 +28,36 @@ export const ProductList = ({
     setCountProducts(countProducts + product.quantity);
     setAllProducts([...allProducts, product]);
   };
+
+  const showModal = (product) => {
+    let modal = document.getElementById(`myModal${product.id}`);
+    modal.style.display = "block";
+  }
+
+  const closeModal = (product) =>{
+    let modal = document.getElementById(`myModal${product.id}`);
+    modal.style.display = "none";
+  }
+
   return (
     <div className="container-items">
       {data.map((product) => (
         <div className="item" key={product.id}>
           <figure>
-            <img src={product.urlImage} alt={product.title}></img>
+            <img src={product.urlImage} alt={product.title} onClick={() => { showModal(product) }}></img>
           </figure>
+          <div id={`myModal${product.id}`} class="modal">
+            <div class="modal-content">
+              <span class="close" onClick={() => {closeModal(product)}}>&times;</span>
+              <h3>Título: {product.title}</h3>
+              <br></br>
+              <b>Descripción:</b>
+              <br></br>
+              <p>{product.descripcion}</p>
+            </div>
+
+          </div>
+
           <div className="info-product">
             <h2 className="titulo-producto-carrito">{product.title}</h2>
             <p className="price">${product.price}</p>
