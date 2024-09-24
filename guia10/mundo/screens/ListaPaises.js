7b71d9f60379d73c78f89d49471efa2559e86035
SomeRandomCatholic <christoamilcruz@gmail.com>
2024-09-23 21:48:46 -0600

    Búsqueda por código

--- guia10/mundo/screens/ListaPaises.js
+++ guia10/mundo/screens/ListaPaises.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { View, Image, Text, TouchableOpacity, FlatList, Dimensions, StyleSheet } from 'react-native';
+import { View, Image, Text, TouchableOpacity, FlatList, Dimensions, StyleSheet, Button, Alert } from 'react-native';
+import { TextInput, TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
 const windowWidth = Dimensions.get('window').width;
 const ListaPaises = ({ navigation }) => {
     const [countries, setCountries] = useState([]);
+    const [terminoBusqueda, setTermino] = useState("");
+    const [paisEncontrado, setPaisEncontrado] = useState();
+    const [bandera, setBandera] = useState(false);
 
     useEffect(() => {
         fetchCountries();
@@ -14,8 +18,26 @@ const ListaPaises = ({ navigation }) => {
             const response = await fetch('https://65f9be823909a9a65b1942ac.mockapi.io/paises');
             const data = await response.json();
             setCountries(data);
+
         } catch (error) {
             console.log("Error fetching countries", error);
+        }
+    }
+
+    const buscar = () => {
+        let resultado = countries.filter(item => item.nombre.espanol == terminoBusqueda.trim())[0];
+
+        if(!resultado){
+            resultado = countries.filter(item => item.codigo_pais == terminoBusqueda.trim())[0];
+        }
+        
+        if (resultado){
+            setPaisEncontrado(resultado);
+            setBandera(true);
+        }
+        else{
+            Alert.alert("Mensaje", "No se encontró el país");
+            setBandera(false);
         }
     }
 
@@ -29,20 +51,38 @@ const ListaPaises = ({ navigation }) => {
     );
 
     return (
-        <View style={styles.container}>
-            <FlatList
-                data={countries}
-                renderItem={renderItem}
-                keyExtractor={(item) => item.id.toString()}
-                contentContainerStyle={styles.countryList}
-                numColumns={2} // Muestra dos países por fila
-            />
+
+        <View style={styles.container} >
+            <View style={styles.containersearch}>
+                <TextInput placeholder='Buscar por país o código' style={styles.textinput} onChangeText={texto => {
+                    setTermino(texto);
+                    if(texto == "")
+                        setBandera(false);
+                }} />
+                <Button title="Buscar" onPress={buscar} />
+            </View>
+            {bandera == true ?
+                (<TouchableOpacity style={styles.countryCard} onPress={() => navigation.navigate("DetallePais", { country: paisEncontrado })}>
+                    <View style={styles.countryInfo}>
+                        <Image source={{ uri: paisEncontrado.bandera }} style={styles.flagImage} />
+                        <Text style={styles.countryName}>{paisEncontrado.nombre.espanol}</Text>
+                    </View>
+                </TouchableOpacity>)
+                :
+                (<FlatList
+                    data={countries}
+                    renderItem={renderItem}
+                    keyExtractor={(item) => item.id.toString()}
+                    contentContainerStyle={styles.countryList}
+                    numColumns={2} // Muestra dos países por fila
+                />)}
+
         </View>
     )
 }
 
 export default ListaPaises;
-// co,
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -73,5 +113,19 @@ const styles = StyleSheet.create({
         fontSize: 16,
         fontWeight: 'bold',
         marginTop: 10,
-    }
+    },
+    containersearch: {
+        margin: 20,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    textinput: {
+        borderRadius: 8,
+        borderWidth: 1,
+        borderColor: '#ddd',
+        height: 40,
+        paddingLeft: 4,
+        flex: 0.9
+    },
+
 });
